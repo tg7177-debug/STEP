f48d42b4b0a72de49988f946e9084e303b02d203
Tanvi Gupta <tg7177@srmist.edu.in>
2026-02-23 18:23:28 +0530

    coded to render OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+/**
+ * OOPSBannerApp UC1 – OOPS Banner Display Application
+ * * This class demonstrates a simple Java application that displays the Object 
+ * Oriented Programming System OOPS acronym to the console.
+ * * @author Tanvi Gupta
+ * @version 1.0
+ */
+
+public class OOPSBannerApp 
+{
+    public static void main(String[] args) 
+    {
+         System.out.println("OOPS");
+    }
+}
